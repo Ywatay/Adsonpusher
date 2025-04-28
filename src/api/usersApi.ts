3691f4e547ebbbf32a2b9173ae798c863f7380fc
Ywatay <akilianchuiko@gmail.com>
2025-04-28 19:52:21 +0300

    add error for axios

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -2,6 +2,15 @@ import axios from "axios";
 import { IUser } from "../store/userStore";
 
 export const fetchUsers = async (): Promise<IUser[]> => {
-  const { data } = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users");
-  return data;
+  try {
+    const { data } = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users");
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error:", error.response?.status, error.message);
+    } else {
+      console.error("Unexpected error:", error);
+    }
+    return [];
+  }
 };
